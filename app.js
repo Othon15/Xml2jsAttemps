10dcfeb6de16c1e1f4e0c657fca8aeb29d6a1189
Othon <othontheocharis@gmail.com>
2022-11-07 12:31:39 +0200

    Second attempt to use xml2js library

--- app.js
+++ app.js
@@ -1,46 +1,49 @@
 //jshint esversion:6
-////////////////////////////EXPRESS/////////////////////
 const express = require ("express");
-
-const app = express();
-/////////////////////////XML BODYPARSER////////////////
-
 const xmlparser = require ("express-xml-bodyparser");
+const app = express();
 
-app.use(xmlparser());
+// const xml2js = require(".routes/xml2js");
 
-/////////////////////////EXPRESS.ROUTER//////////////
+app.set("view engine","pug");
 
-const router = express.Router();                    
-
-//const xml2js = require ("./xml2js/customer");
-
-//app.use("/xml2js/customer",xml2js);
-
-app.use(require("./routes"));        // epikoinwnei me to directory routes 
-
-///////xml2js////////
+app.use(xmlparser({
+    normalizeTags: false
+}));
 
 var parseString = require('xml2js').parseString;
 var xml = "<root>Hello xml2js!</root>"
 parseString(xml, function (err, result) {
     console.dir(result);
 });
+// app.set("/xml2js",xml2js);          //to paradeigma elege use alla to kaname set(marina) kai leitourgise!
+// app.use(require("./routes"));           
 
-///////////////////////////////PUG///////////////////
+// app.use("/xml2js",xml2js);
 
-const pug = require ("pug");
-
-app.set("view engine","pug");
-
-////////////////////////////////////////////////////////
-
-
+app.get("/",function (req,res){
+    res.render("home");
+});
 
 
 
+app.get("/xml2js",function(req,res){
+    res.render("xml2js")
+});
+
+app.post('/xml2js/customer', function(req, res, next) {
+    console.log('Raw XML: ' + req.rawBody);
+    console.log('Parsed XML: ' + JSON.stringify(req.body));
+    if (req.body.retrieveCustomer) {
+      var id = req.body.retrieveCustomer.id;
+      res.send(`<customer><id>${id}</id><fullName>Bob Smith</fullName></customer>`);
+    } else {
+      res.status(400).send('Unexpected XML received, missing <retrieveCustomer>');
+    }
+  });
 
 
-  app.listen(3000,function(){
+  
+app.listen(3000,function(){
     console.log("Othon,Server started at localhost 3000!");
   });
---
+++ routes/xml2js.js
@@ -0,0 +1,26 @@
+// //jshint esversion:6
+// const express = require ("express");
+// const router = express.Router();
+
+
+// router.get("/xml2js",function(req,res){
+//   res.send("xml2js home page");
+// });
+
+
+// // router.post('/xml2js/customer', function(req, res, next) {
+// //     console.log('Raw XML: ' + req.rawBody);
+// //     console.log('Parsed XML: ' + JSON.stringify(req.body));
+// //     if (req.body.retrievecustomer) {
+// //       var id = req.body.retrievecustomer.id;
+// //       res.send(`<customer><id>${id}</id><fullName>Bob Smith</fullName></customer>`);
+// //     } else {
+// //       res.status(400).send('Unexpected XML received, missing <retrieveCustomer>');
+// //     }
+// //   });
+
+
+
+
+// module.exports = router ;
+// export default xml2js
