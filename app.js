37ca70d733fe1ac0d6248bd8f646650b6d2dbf93
Othon <othontheocharis@gmail.com>
2022-11-07 17:07:39 +0200

    First attempt to use libxmljs2 library

--- app.js
+++ app.js
@@ -2,24 +2,24 @@
 const express = require ("express");
 const xmlparser = require ("express-xml-bodyparser");
 const app = express();
+var path = require('path');
+var libxmljs2Router = require('./routes/libxmljs2');
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
 
-// const xml2js = require(".routes/xml2js");
-
-app.set("view engine","pug");
+app.use('/routes/libxmljs2',libxmljs2Router);
 
 app.use(xmlparser({
     normalizeTags: false
 }));
 
-var parseString = require('xml2js').parseString;
-var xml = "<root>Hello xml2js!</root>"
-parseString(xml, function (err, result) {
-    console.dir(result);
+ var parseString = require('xml2js').parseString;
+ var xml = "<root>Hello xml2js!</root>"
+ parseString(xml, function (err, result) {
+     console.dir(result);
 });
-// app.set("/xml2js",xml2js);          //to paradeigma elege use alla to kaname set(marina) kai leitourgise!
-// app.use(require("./routes"));           
 
-// app.use("/xml2js",xml2js);
+
 
 app.get("/",function (req,res){
     res.render("home");
@@ -32,18 +32,15 @@ app.get("/xml2js",function(req,res){
 });
 
 app.post('/xml2js/customer', function(req, res, next) {
-    console.log('Raw XML: ' + req.rawBody);
-    console.log('Parsed XML: ' + JSON.stringify(req.body));
-    if (req.body.retrieveCustomer) {
-      var id = req.body.retrieveCustomer.id;
-      res.send(`<customer><id>${id}</id><fullName>Bob Smith</fullName></customer>`);
-    } else {
-      res.status(400).send('Unexpected XML received, missing <retrieveCustomer>');
-    }
-  });
+  console.log('Raw XML: ' + req.rawBody);
+  console.log('Parsed XML: ' + JSON.stringify(req.body));
+  if (req.body.retrieveCustomer) {
+    var id = req.body.retrieveCustomer.id;
+    res.send(`<customer><id>${id}</id><fullName>Bob Smith</fullName></customer>`);
+  } else {
+    res.status(400).send('Unexpected XML received, missing <retrieveCustomer>');
+  }
+});
 
 
-  
-app.listen(3000,function(){
-    console.log("Othon,Server started at localhost 3000!");
-  });
+module.exports = app;
---
+++ routes/libxmljs2.js
@@ -0,0 +1,42 @@
+//jshint esversion:6
+var express = require('express');
+var router = express.Router();
+var libxmljs = require('libxmljs2');
+var fs = require('fs');
+var path = require('path');
+
+var router = express.Router();
+
+
+router.get('/libxmljs2', function(req, res, next) {
+    res.render('libxmljs2', {});
+  });
+
+router.post('/libxmljs2/validateSessionXml', (req, res, next) => {
+  var xmlData = req.body;
+
+  // parse incoming XML data
+  var xmlDoc = libxmljs.parseXml(xmlData);  
+
+  // load XML schema from file system
+  var xmlSchemaDoc = loadXmlSchema('session-info.xsd');
+
+  // validate XML data against schema
+  var validationResult = xmlDoc.validate(xmlSchemaDoc);
+
+  // return success or failure with validation errors
+  if (validationResult) {
+    res.status(200).send('validation successful');
+  } else {
+    res.status(400).send(`${xmlDoc.validationErrors}`);
+  }  
+});
+
+function loadXmlSchema(filename) {
+  var schemaPath = path.join(__dirname, '..', 'schemas', filename);
+  var schemaText = fs.readFileSync(schemaPath, 'utf8');
+  return libxmljs.parseXml(xmlSchema); 
+}
+
+
+module.exports = router;
